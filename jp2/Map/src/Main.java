c89bfcd05cc02de3c50c2109a8747282a3ac08d3
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2024-10-27 23:32:36 +0700

    e1

---
+++ jp2/Map/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(120);
+        set.add(121);
+        set.add(122);
+        set.add(123);
+        set.add(124);
+        set.add(125);
+        set.add(126);
+        set = set.stream().filter(Main::divisionByThree).collect(Collectors.toSet());
+        System.out.println(set);
+    }
+
+    private static Boolean divisionByThree(int num) {
+        return Integer.remainderUnsigned(num, 3) == 0;
+    }
+}
